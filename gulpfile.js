bd5f397c720ca274e1be170d9e30a5c3ee999852
Lukáš Fabik <fabik@befirst.cz>
2018-01-21 20:13:30 +0100

    add styles to documentation

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,20 @@ gulp.task("scss", function () {
         .pipe(gulp.dest("data/css"))
 });
 
+// Compile SCSS files to CSS for Documentation
+gulp.task("scss-doc", function () {
+    //del(["static/_assets/css/www/**/*"])
+    gulp.src("src/scss/main-doc.scss")
+        .pipe(sass({outputStyle : "compressed"}))
+        .pipe(autoprefixer({browsers : ["last 20 versions"]}))
+        .pipe(hash())
+        .pipe(gulp.dest("static/_assets/css"))
+        //Create a hash map
+        .pipe(hash.manifest("hash.json"))
+        //Put the map in the data directory
+        .pipe(gulp.dest("data/css"))
+});
+
 // Hash javascript
 gulp.task("js", function () {
     //del(["static/_assets/js/www/**/*"])
@@ -46,7 +60,7 @@ gulp.task("js", function () {
 
 
 // Watch asset folder for changes
-gulp.task("watch", ["scss", "js"], function () {
+gulp.task("watch", ["scss", "scss-doc", "js"], function () {
     gulp.watch("src/scss/**/*", ["scss"])
     gulp.watch("src/js/**/*", ["js"])
 });
